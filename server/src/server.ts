dd69014388a425d6733715da948bf14db93d96a6
Lucas Lopes <lucas.lopes@infracommerce.com.br>
2020-06-05 18:45:45 -0300

    Create apis to manage points and items

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Teste');
-
-    response.json([
-        'Lucas',
-        'Jose',
-        'Maria',
-        'Mario'
-
-    ]);
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
